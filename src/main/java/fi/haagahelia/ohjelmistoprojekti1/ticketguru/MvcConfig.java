d35c3ed89a73a5b4ea7915d229471577d60bffb9
Ruonavaara Markku <markku.ruonavaara@haaga-helia.fi>
2022-03-16 09:04:31 +0200

    Security- ja CORS-konfiguraatioita muutettu, ei kauhean siisti

--- src/main/java/fi/haagahelia/ohjelmistoprojekti1/ticketguru/MvcConfig.java
+++ src/main/java/fi/haagahelia/ohjelmistoprojekti1/ticketguru/MvcConfig.java
@@ -3,13 +3,12 @@ package fi.haagahelia.ohjelmistoprojekti1.ticketguru;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+/*
 @Configuration
-//@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
-	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**").allowedOrigins("http://127.0.0.1:1000");
 	}
 }
+*/
--- src/main/java/fi/haagahelia/ohjelmistoprojekti1/ticketguru/WebSecurityConfiguration.java
+++ src/main/java/fi/haagahelia/ohjelmistoprojekti1/ticketguru/WebSecurityConfiguration.java
@@ -1,14 +1,19 @@
 package fi.haagahelia.ohjelmistoprojekti1.ticketguru;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 import fi.haagahelia.ohjelmistoprojekti1.ticketguru.controller.DetailsService;
 import fi.haagahelia.ohjelmistoprojekti1.ticketguru.model.User;
@@ -17,23 +22,39 @@ import fi.haagahelia.ohjelmistoprojekti1.ticketguru.model.User;
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
-    DetailsService detailsService;
+	DetailsService detailsService;
 
 	@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(detailsService).passwordEncoder(User.PASSWORD_ENCODER);
-    }
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(detailsService).passwordEncoder(User.PASSWORD_ENCODER);
+	}
 
-    @Override
+	@Override
     protected void configure(HttpSecurity http) throws Exception {
     	//http.headers().frameOptions().sameOrigin(); // h2-console needs this
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
         http.authorizeRequests()
-        		//.antMatchers("/").permitAll()
         		.anyRequest().authenticated()
-                .and().cors()
                 .and().httpBasic()
                 .and().csrf().disable();
-                /*.headers().disable()*/;
-    }
     
+        http.cors().configurationSource(this.corsConfigurationSource());
+        //http.cors();
+
+	}
+	
+    // @Bean
+    CorsConfigurationSource corsConfigurationSource() 
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
+    }
+
 }
