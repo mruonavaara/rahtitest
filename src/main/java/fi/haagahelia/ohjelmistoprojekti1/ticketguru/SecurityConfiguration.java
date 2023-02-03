7f8b3f161e1856f2cf95033942adc169a526de6f
Ruonavaara Markku <markku.ruonavaara@haaga-helia.fi>
2023-02-03 21:38:44 +0200

    CORS-konfiguraatiot taas uusiksi, kaipaa vieläkin selvitystä ja selkiytystä, miksi oli niin vaikea saada toimimaan

---
+++ src/main/java/fi/haagahelia/ohjelmistoprojekti1/ticketguru/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package fi.haagahelia.ohjelmistoprojekti1.ticketguru;
+
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated().and().httpBasic();
+
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
